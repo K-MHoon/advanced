639747c88a4f0bec4da66eebe4db13119cb5ab8c
K-MHoon <azmria117@gmail.com>
2021-12-16 14:04:05 +0900

    feat : 인터페이스 있는 구현 클래스에 적용 (V1)
    - 원본 코드 수정 없이 로그 추적기 도입
    - Client > Proxy(Controller) > 구현체(ControllerV1Impl) >
    - Proxy(Service) > 구현체(ServiceV1Impl)
    - Proxy(Repository) > 구현체(RepositoryV1Impl)
    - 로 접근하는 형식을 구현.
    - 스프링 빈에는 프록시가 저장되어 있음.

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -2,16 +2,26 @@ package hello.advanced;
 
 import hello.advanced.proxy.config.AppV1Config;
 import hello.advanced.proxy.config.AppV2Config;
+import hello.advanced.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import({AppV1Config.class, AppV2Config.class}) // 클래스를 스프링 빈으로 등록한다.
+//@Import({AppV1Config.class, AppV2Config.class}) // 클래스를 스프링 빈으로 등록한다.
+@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.advanced.proxy.app")
 public class AdvancedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdvancedApplication.class, args);
+	}
+
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
 	}
 
 }
