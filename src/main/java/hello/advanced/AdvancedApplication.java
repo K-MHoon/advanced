5cc539493e58ed4b507bd37aeedf7d81425c7ce9
K-MHoon <azmria117@gmail.com>
2021-12-16 10:55:34 +0900

    feat : V2(수동 빈 등록), V3(컴포넌트 스캔 등록) 예제 추가

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -1,11 +1,12 @@
 package hello.advanced;
 
 import hello.advanced.proxy.config.AppV1Config;
+import hello.advanced.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class) // 클래스를 스프링 빈으로 등록한다.
+@Import({AppV1Config.class, AppV2Config.class}) // 클래스를 스프링 빈으로 등록한다.
 @SpringBootApplication(scanBasePackages = "hello.advanced.proxy.app")
 public class AdvancedApplication {
 
---
+++ src/main/java/hello/advanced/proxy/app/v2/OrderControllerV2.java
@@ -0,0 +1,30 @@
+package hello.advanced.proxy.app.v2;
+
+import hello.advanced.proxy.app.v1.OrderControllerV1;
+import hello.advanced.proxy.app.v1.OrderServiceV1;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping
+@ResponseBody
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v2/no-log")
+    public String noLog() {
+        return "ok";
+
+    }
+}
